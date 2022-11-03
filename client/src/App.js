2313270c5a35034f4a9c1c2dcca57b236d4dcdb4
Appoline Cogan <appolinecogan@gmail.com>
2022-11-03 21:34:57 +1100

    imported all components to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-
+import MainPageHero from "./components/MainPageHero";
+import Footer from "./components/Footer";
+import MainBodyPage from "./components/MainPageBody";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
     <ChakraProvider>
       <NavBar />
+      <MainPageHero />
+      <MainBodyPage />
+      <Footer />
     </ChakraProvider>
   );
 }
