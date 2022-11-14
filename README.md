211b09f35c62762501ca6bf2eba331acc2efc135
Appoline Cogan <appolinecogan@gmail.com>
2022-11-14 13:18:50 +1100

    redeploying

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Abortion-saves-lives
+
+## Table of content
+
+- [Description of Project](#description-of-project)
+- [Project Requirements](#project-requirements)
+- [What I have learned](#what-i-have-learned)
+- [Challenges](#challenges)
+- [Link to deployed application](#link-to-deployed-application)
+- [Screenshot of deployed application](#screenshot-of-deployed-application)
+
+## Description of Project
+
+This project is a single-page application portfolio using React.
+
+## Project Requirements
+
+Here are the main requirements of the project:
+
+
+## What I have learned 
+
+
+## Challenges
+
+
+### Link to deployed application
+
+[Link to deployed application]()
+
+### Screenshot of deployed application
+
+![Screenshot of application](./src/images/deployed-app.png)
+
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,9 @@
 const { User, Facility } = require("../models");
 const { signToken } = require("../utils/auth");
 const { AuthenticationError } = require("apollo-server-express");
-const stripe = require("stripe")(
-  "sk_test_51M2T2pDrKBgAeypOM27rcIo6LiLp11LDExrygBYkEC8HiN3hSDu4p4HG22NBREuallrjjghXtLZwKfXz6glSsQmK00Mp2l7uER"
-);
+// const stripe = require("stripe")(
+//   "sk_test_51M2T2pDrKBgAeypOM27rcIo6LiLp11LDExrygBYkEC8HiN3hSDu4p4HG22NBREuallrjjghXtLZwKfXz6glSsQmK00Mp2l7uER"
+// );
 
 const resolvers = {
   Query: {
@@ -26,25 +26,25 @@ const resolvers = {
       throw new AuthenticationError("You need to be logged in!");
     },
 
-    checkout: async (parent, args, context) => {
-      const url = new URL(context.headers.referer).origin;
+    // checkout: async (parent, args, context) => {
+    //   const url = new URL(context.headers.referer).origin;
 
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: "{{price_1M2oxADrKBgAeypOs6fLbDG1}}",
-            quantity: 1,
-          },
-        ],
-        mode: "payment",
-        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${url}/`,
-      });
+    //   const session = await stripe.checkout.sessions.create({
+    //     payment_method_types: ["card"],
+    //     line_items: [
+    //       {
+    //         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+    //         price: "{{price_1M2oxADrKBgAeypOs6fLbDG1}}",
+    //         quantity: 1,
+    //       },
+    //     ],
+    //     mode: "payment",
+    //     success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+    //     cancel_url: `${url}/`,
+    //   });
 
-      return { session: session.id };
-    },
+    //   return { session: session.id };
+    // },
   },
 
   Mutation: {
@@ -89,39 +89,26 @@ const resolvers = {
       throw new AuthenticationError("You need to be logged in!");
     },
 
-    removeReview: async (parent, { facilityId, reviewId }) => {
-      // if (context.user) {
-      return Facility.findOneAndUpdate(
-        { _id: facilityId },
-        {
-          $pull: {
-            reviews: { _id: reviewId },
-          },
-        },
-        { new: true }
-      );
-      // }
-      // throw new AuthenticationError("You need to be logged in!");
-    },
+    // removeReview: async (parent, { facilityId, reviewId }) => {
+    //   // if (context.user) {
+    //   return Facility.findOneAndUpdate(
+    //     { _id: facilityId },
+    //     {
+    //       $pull: {
+    //         reviews: { _id: reviewId },
+    //       },
+    //     },
+    //     { new: true }
+    //   );
+    //   // }
+    //   // throw new AuthenticationError("You need to be logged in!");
+    // },
 
-    removeUser: async (parent, { userId }) => {
-      return User.findOneAndDelete({ _id: userId });
-    },
+    // removeUser: async (parent, { userId }) => {
+    //   return User.findOneAndDelete({ _id: userId });
+    // },
   },
 };
 
 
 module.exports = resolvers;
-
-//     removeComment: async (parent, { commentId }) => {
-//       return Comment.findOneAndDelete({ _id: commentId });
-//     },
-
-//     removeReview: async (parent, { facilityId, reviewId }) => {
-//       return Facility.findOneAndUpdate(
-//         { _id: facilityId },
-//         { $pull: { reviews: { _id: reviewId } } },
-//         { new: true }
-//       );
-// };
-
